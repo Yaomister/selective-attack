6bac870e83885d5e55a91ab78ad4d00210600abf
Eric Yao <ericyaohan@yahoo.com>
2026-09-14 19:17:15 -0400

    adjusted default epsilon

--- scripts/experiment_sweep_layer_from_last.sh
+++ scripts/experiment_sweep_layer_from_last.sh
@@ -20,7 +20,7 @@ python experiments/experiment_v3.py \
   --dataset_dir ./sorted \
   --output_dir ./attack_results/pooling_method_$LAYER \
   --steps 200 \
-  --epsilon 1 \
+  --epsilon 0.025 \
   --alpha 0.001 \
   --mu 10 \
   --layer_from_last $LAYER \
